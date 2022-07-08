e85afc207138f2ebb9b3fd7e732ba1be3756325d
Risu Singh <risusingh35@gmail.com>
2022-07-08 10:33:19 +0530

    First Commit
    textArea Input and button component

---
+++ example/widgets/index.js
@@ -0,0 +1,9 @@
+import braidButton from './button/index.vue'
+import braidInput from './input/index.vue'
+import braidtextArea from './textArea/index.vue'
+
+export default {
+  [braidButton.name]: braidButton,
+  [braidInput.name]: braidInput,
+  [braidtextArea.name]: braidtextArea,
+}
